1fd63036aa5ec019989f9c6663070dffbec47b92
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-10-23 13:27:20 -0500

    adding gallery section

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import About from "./components/About/About";
+import LandingPage from "./components/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      <About />
+    </>
+  );
+}
+
+export default App;
