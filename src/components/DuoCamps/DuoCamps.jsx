497512e2e8da039e793b0af2dc7b2b892992e695
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-10-23 23:11:16 -0500

    landmarks

--- src/components/DuoCamps/DuoCamps.jsx
+++ src/components/DuoCamps/DuoCamps.jsx
@@ -5,30 +5,36 @@ import Box from "@mui/material/Box";
 export default function DuoCamps() {
   return (
     <div id="duoCamps">
-      <Box
-        component="img"
-        sx={{
-          height: 500,
-          width: 750,
-          maxHeight: { xs: 233, md: 450 },
-          maxWidth: { xs: 350, md: 650 },
-        }}
-        alt="The house from the offer."
-        src="src\assets\mountains.jpg"
-        className="landscapes"
-      />
-      <Box
-        component="img"
-        sx={{
-          height: 500,
-          width: 750,
-          maxHeight: { xs: 233, md: 450 },
-          maxWidth: { xs: 350, md: 650 },
-        }}
-        alt="Lake Views"
-        src="src\assets\lake.jpg"
-        className="landscapes"
-      />
+      <div>
+        <Box
+          component="img"
+          sx={{
+            height: 500,
+            width: 750,
+            maxHeight: { xs: 233, md: 450 },
+            maxWidth: { xs: 350, md: 650 },
+          }}
+          alt="The house from the offer."
+          src="src\assets\mountains.jpg"
+          className="landscapes"
+        ></Box>
+        <h1 className="landscapesPictures">Mountains</h1>
+      </div>
+      <div>
+        <Box
+          component="img"
+          sx={{
+            height: 500,
+            width: 750,
+            maxHeight: { xs: 233, md: 450 },
+            maxWidth: { xs: 350, md: 650 },
+          }}
+          alt="Lake Views"
+          src="src\assets\lake.jpg"
+          className="landscapes"
+        />
+        <h1 className="landscapesPictures">Lake</h1>
+      </div>
     </div>
   );
 }
