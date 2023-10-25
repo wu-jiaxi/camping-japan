3568bd40901280ae9de56f6aa19b88b127b6e04e
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-10-24 21:13:15 -0500

    box

--- src/components/DuoCamps/DuoCamps.jsx
+++ src/components/DuoCamps/DuoCamps.jsx
@@ -2,66 +2,39 @@ import { Container } from "@mui/material";
 import "./DuoCamps.css";
 import Box from "@mui/material/Box";
 
-import CardMedia from "@mui/material/CardMedia";
-
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-
 export default function DuoCamps() {
   return (
     <div id="duoCamps">
       <div>
         <div className="head-text">
-          <Card className="cardContainer">
-            <div>
-              <CardContent id="mountainCardContent">
-                <div style={{ position: "relative" }}>
-                  <CardMedia component="img" image="src\assets\mountains.jpg" />
-                  <div
-                    style={{
-                      position: "absolute",
-                      color: "white",
-                      top: "50%",
-                      left: "50%",
-                      transform: "translateX(-50%)",
-                    }}
-                  >
-                    Your text
-                  </div>
-                </div>
-              </CardContent>
-            </div>
-          </Card>
-        </div>
-        <div className="text-Image">
-          <h1>Mountains</h1>
+          <Box
+            component="img"
+            sx={{
+              height: 500,
+              width: 750,
+              maxHeight: { xs: 233, md: 450 },
+              maxWidth: { xs: 350, md: 650 },
+            }}
+            alt="Lake Views"
+            src="src\assets\lake.jpg"
+            className="landscapes"
+          ></Box>
         </div>
       </div>
       <div>
         <div className="head-text">
-          <Card className="cardContainer">
-            <div>
-              <CardContent>
-                <div style={{ position: "relative" }}>
-                  <CardMedia component="img" image="src\assets\lake.jpg" />
-                  <div
-                    style={{
-                      position: "absolute",
-                      color: "white",
-                      top: "50%",
-                      left: "50%",
-                      transform: "translateX(-50%)",
-                    }}
-                  >
-                    Your text
-                  </div>
-                </div>
-              </CardContent>
-            </div>
-          </Card>
+          <Box
+            component="img"
+            sx={{
+              height: 500,
+              width: 750,
+              maxHeight: { xs: 233, md: 450 },
+              maxWidth: { xs: 350, md: 650 },
+            }}
+            alt="The house from the offer."
+            src="src\assets\mountains.jpg"
+            className="landscapes"
+          />
         </div>
       </div>
     </div>
