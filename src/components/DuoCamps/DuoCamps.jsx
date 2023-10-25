55ac9376871a140535af15d164e242b893ea5cc2
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-10-24 21:38:36 -0500

    card section text overlay is working

--- src/components/DuoCamps/DuoCamps.jsx
+++ src/components/DuoCamps/DuoCamps.jsx
@@ -1,40 +1,60 @@
-import { Container } from "@mui/material";
 import "./DuoCamps.css";
-import Box from "@mui/material/Box";
+import CardMedia from "@mui/material/CardMedia";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
 
 export default function DuoCamps() {
   return (
     <div id="duoCamps">
       <div>
         <div className="head-text">
-          <Box
-            component="img"
-            sx={{
-              height: 500,
-              width: 750,
-              maxHeight: { xs: 233, md: 450 },
-              maxWidth: { xs: 350, md: 650 },
-            }}
-            alt="Lake Views"
-            src="src\assets\lake.jpg"
-            className="landscapes"
-          ></Box>
+          <Card>
+            <div>
+              <CardContent>
+                <div style={{ position: "relative" }}>
+                  <CardMedia component="img" image="src\assets\mountains.jpg" />
+                  <div
+                    style={{
+                      position: "absolute",
+                      color: "white",
+                      top: 8,
+                      left: "50%",
+                      transform: "translateX(-50%)",
+                    }}
+                  >
+                    <h1>Mountain</h1>
+                    <Button variant="contained">Learn More</Button>
+                  </div>
+                </div>
+              </CardContent>
+            </div>
+          </Card>
         </div>
       </div>
       <div>
         <div className="head-text">
-          <Box
-            component="img"
-            sx={{
-              height: 500,
-              width: 750,
-              maxHeight: { xs: 233, md: 450 },
-              maxWidth: { xs: 350, md: 650 },
-            }}
-            alt="The house from the offer."
-            src="src\assets\mountains.jpg"
-            className="landscapes"
-          />
+          <Card style={{ width: "85%" }}>
+            <div>
+              <CardContent>
+                <div style={{ position: "relative" }}>
+                  <CardMedia component="img" image="src\assets\lake.jpg" />
+                  <div
+                    style={{
+                      position: "absolute",
+                      color: "white",
+                      top: 8,
+                      left: "50%",
+                      transform: "translateX(-50%)",
+                    }}
+                  >
+                    <h1>Lake</h1>
+                    <Button variant="contained">Learn More</Button>
+                  </div>
+                </div>
+              </CardContent>
+            </div>
+          </Card>
         </div>
       </div>
     </div>
