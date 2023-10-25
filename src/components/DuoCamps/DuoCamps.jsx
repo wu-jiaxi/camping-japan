bd65f798513fc01eb19d7fcec9986af24ea24cf0
Jiaxi Wu <61803844+Jiaxi-Wu-Dev@users.noreply.github.com>
2023-10-24 23:27:30 -0500

    centered text on image

--- src/components/DuoCamps/DuoCamps.jsx
+++ src/components/DuoCamps/DuoCamps.jsx
@@ -3,60 +3,57 @@ import CardMedia from "@mui/material/CardMedia";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 export default function DuoCamps() {
   return (
     <div id="duoCamps">
-      <div>
-        <div className="head-text">
-          <Card>
-            <div>
-              <CardContent>
-                <div style={{ position: "relative" }}>
-                  <CardMedia component="img" image="src\assets\mountains.jpg" />
-                  <div
-                    style={{
-                      position: "absolute",
-                      color: "white",
-                      top: 8,
-                      left: "50%",
-                      transform: "translateX(-50%)",
-                    }}
-                  >
-                    <h1>Mountain</h1>
-                    <Button variant="contained">Learn More</Button>
-                  </div>
-                </div>
-              </CardContent>
+      <Card className="head-text">
+        <div>
+          <CardContent>
+            <div style={{ position: "relative" }}>
+              <CardMedia component="img" image="src\assets\mountains1.jpg" />
+              <div
+                style={{
+                  position: "absolute",
+                  color: "white",
+                  top: "33%",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                }}
+                className="textButton"
+              >
+                <h1>Mountain</h1>
+                <Button variant="contained">Learn More</Button>
+              </div>
             </div>
-          </Card>
+          </CardContent>
         </div>
-      </div>
-      <div>
-        <div className="head-text">
-          <Card style={{ width: "85%" }}>
-            <div>
-              <CardContent>
-                <div style={{ position: "relative" }}>
-                  <CardMedia component="img" image="src\assets\lake.jpg" />
-                  <div
-                    style={{
-                      position: "absolute",
-                      color: "white",
-                      top: 8,
-                      left: "50%",
-                      transform: "translateX(-50%)",
-                    }}
-                  >
-                    <h1>Lake</h1>
-                    <Button variant="contained">Learn More</Button>
-                  </div>
-                </div>
-              </CardContent>
+      </Card>
+
+      <Card className="head-text">
+        <div>
+          <CardContent>
+            <div style={{ position: "relative" }}>
+              <CardMedia component="img" image="src\assets\lake1.jpg" />
+              <div
+                style={{
+                  position: "absolute",
+                  color: "white",
+                  top: "33%",
+                  left: "50%",
+
+                  transform: "translateX(-50%)",
+                }}
+                className="textButton"
+              >
+                <h1 id="lakeTitle">Lake</h1>
+                <Button variant="contained">Learn More</Button>
+              </div>
             </div>
-          </Card>
+          </CardContent>
         </div>
-      </div>
+      </Card>
     </div>
   );
 }
